1ed69f828bcf9e3ded6ff2eeb9571cc8e2773395
20180310127ling <58196840+20180310127ling@users.noreply.github.com>
2019-12-17 13:43:44 +0800

    扑克

---
+++ DataStructuresAndAlgorithm/鸡/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 鸡
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //i:公鸡 j:母鸡 z:小鸡
+            for (int j = 0; j <= 20; j++)
+            {
+                for (int i = 0; i <= 33; i++)
+                {
+                    int z = 100 - j - i;
+                    if ((j * 5 + i * 3 + z / 3 == 100) && z % 3 == 0)
+                    {
+                        Console.WriteLine("公鸡:{0},母鸡:{1},小鸡:{2}", j, i, z);
+                    }
+                }
+            }
+            Console.Read();
+        }
+    }
+}
